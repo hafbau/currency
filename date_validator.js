90fda13f9cbe8c741f66b7f8a44a8736ab513762
Hafiz Suara <hafbau@yahoo.com>
2017-03-02 02:56:13 +0000

    converted date_validator to es6

--- date_validator.js
+++ date_validator.js
@@ -5,10 +5,10 @@ module.exports = function (date) {
   ** uses regex to avoid the unpredictability
   ** around Date.parse() or new Date() interpretation
   */
-  var regex = /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/;
-  var validDateMatch = date.match(regex);
-  var year = Number(date.slice(0,4))
-  var today = new Date();
+  const regex = /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/;
+  const validDateMatch = date.match(regex);
+  const year = Number(date.slice(0,4))
+  const today = new Date();
 
   return (year <= today.getFullYear()) && (validDateMatch);
 }
